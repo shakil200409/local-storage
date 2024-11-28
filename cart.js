62c3612328d9f9eb1c4b06297f668e2a4655e4c6
Shakil Ahmed <shakil200409@gmail.com>
2024-11-28 23:07:45 +0600

    Tried to display the stored data of local storage

---
+++ cart.js
@@ -0,0 +1,29 @@
+const addProduct = () => {
+  const productField = document.getElementById("product-name");
+  const quantityField = document.getElementById("product-quantity");
+  const product = productField.value;
+  const quantity = quantityField.value;
+  productField.value = "";
+  quantityField.value = "";
+  console.log(product, quantity);
+  displayProduct(product, quantity);
+};
+
+const displayProduct = (product, quantity) => {
+  const ul = document.getElementById("product-container");
+  const li = document.createElement("li");
+
+  li.innerText = `${product}: ${quantity}`;
+
+  ul.appendChild(li);
+};
+
+function getShoppingCart(){
+    let cart = {};
+    const shoppingCart = localStorage.getItem('cart');
+    if(shoppingCart){
+        cart = JSON.parse(shoppingCart)
+        // cart = JSON.parse(shoppingCart);
+    }
+    return cart;
+}
